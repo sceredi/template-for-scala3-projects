8d7f1bcf1698ed7d09f6f8c120acced8c976767f
sceredi <ceredi.simone.iti@gmail.com>
2025-04-09 15:57:36 +0200

    feat: add automatic release configuration

---
+++ release.config.mjs
@@ -0,0 +1,21 @@
+import config from "semantic-release-preconfigured-conventional-commits" with { type: "json" };
+
+const publishCmd = `
+echo "HAS_RELEASED=true" >> $GITHUB_ENV
+echo "RELEASE_VERSION="\${nextRelease.version} >> $GITHUB_ENV
+git tag -a -f v\${nextRelease.version} v\${nextRelease.version} -F CHANGELOG.md  || exit 1
+export CI_COMMIT_TAG="true"
+`;
+config.plugins.push(
+  [
+    "@semantic-release/exec",
+    {
+      publishCmd: publishCmd,
+    },
+  ],
+  "@semantic-release/github",
+  "@semantic-release/git",
+);
+config.tagFormat = "v${version}";
+
+export default config;
